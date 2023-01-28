5db458fe402668e797ea979989080356381200e4
Alexandra Paxton <paxton.alexandra@gmail.com>
2023-01-27 23:36:24 -0500

    Streamline video processing

--- scripts/02-data_processing/process_one_movie.py
+++ scripts/02-data_processing/process_one_movie.py
@@ -2,9 +2,7 @@
 
 # # Visual Information in Coordination: Video processing
 
-# This script is adapted from analyses from Paxton et al. (2018,
-# Cognitive Science Society), based on Paxton & Dale
-# (2013, Behavior Research Methods).
+# This script extracts basic movement from frame-difference measures.
 
 # ***
 
@@ -23,14 +21,15 @@ print('Python received: File number '+str(task_number))
 # move into our data folder
 os.chdir('data')
 
-# figure out what our next file is
-video_files = glob.glob('/home/aep17003/visual_information_in_coordination/videos-raw/*.wmv')
+# figure out what our next file is (glob hat tip to https://stackoverflow.com/a/26403164)
+video_files = glob.glob('/home/aep17003/visual_information_in_coordination/data/videos/*.wmv')
+video_files.extend(glob.glob('/home/aep17003/visual_information_in_coordination/data/videos/*.mp4'))
 next_video_file = video_files[task_number]
-print(next_video_file+ ': Video file processing')
 
 # grab our conversation identifier
 conv_id = os.path.basename(next_video_file) # movie
 conv = os.path.splitext(conv_id)[0]
+print(conv + ': Video file processing')
 
 # ***
 
@@ -49,41 +48,29 @@ return_frame_value, frame1 = video_capture.read()
 previous_frame = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
 previous_frame = cv2.bilateralFilter(previous_frame,9,75,75)
 
-# get image dimensions
-image_dimensions = previous_frame.shape
-single_person_height = image_dimensions[0]
-single_person_width = round(image_dimensions[1]/3)
-# middle_gap = image_dimensions[1] - (single_person_width*2)
+# specify height (without bottom edge because of timers)
+person_height_start = 0
+person_height_stop = 980
 
-# outline the two participants' spaces
-person_left_x_start = 0 + round(image_dimensions[1]*.09)
-person_left_x_end = person_left_x_start + single_person_width
-person_right_x_start = image_dimensions[1] - round(image_dimensions[1]*.09) - single_person_width
-person_right_x_end = image_dimensions[1] - round(image_dimensions[1]*.09)
-
-# specify left participant's matrix
-person_left_rows = np.array(range(single_person_height))
-person_left_columns = np.array(range(person_left_x_start,
-                                     person_left_x_end))
-
-# specify right participant's matrix
-person_right_rows = np.array(range(single_person_height))
-person_right_columns = np.array(range(person_right_x_start,
-                                      person_right_x_end))
-
+# specify width based on Zoom or not
+person_left_x_start = 0
+if "_Z" in conv: 
+    person_left_x_end = 950
+    person_right_x_start = 970
+else:
+    person_left_x_end = 700
+    person_right_x_start = 1250
+person_right_x_end = 1920
+    
 # reset difference counter and dataframes
 difference_number = 0;
 dyad_differenced_dataframe = pd.DataFrame()
 participant_differenced_dataframe = pd.DataFrame()
 
-# loop through frames
-n = 0
-while(n < 100):
-
-    # read in next frame
-    n += 1
-    return_frame_value, frame2 = video_capture.read()
-
+# loop through frames (if frame exists)
+return_frame_value, frame2 = video_capture.read()
+while return_frame_value:
+    
     # read it in if there's something left to read
     if (return_frame_value == True):
         next_frame = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
@@ -93,13 +80,15 @@ while(n < 100):
         differenced_frame = np.matrix(cv2.absdiff(next_frame, previous_frame))
 
         # save entire frame's columnwise mean
-        whole_frame_movement = differenced_frame.mean(axis=1)
+        whole_frame_matrix = differenced_frame[person_height_start:person_height_stop,
+                                               :]
+        whole_frame_movement = whole_frame_matrix.mean(axis=1)
 
         # carve up left and right matrices
-        person_left_matrix = differenced_frame[person_left_rows[:, None],
-                                               person_left_columns]
-        person_right_matrix = differenced_frame[person_right_rows[:, None],
-                                                person_right_columns]
+        person_left_matrix = differenced_frame[person_height_start:person_height_stop,
+                                               person_left_x_start:person_left_x_end]
+        person_right_matrix = differenced_frame[person_height_start:person_height_stop,
+                                                person_right_x_start:person_right_x_end]
 
         # get average movement for each person
         person_left_movement = person_left_matrix.mean()
@@ -109,40 +98,96 @@ while(n < 100):
         difference_number = difference_number + 1
 
         # add the current data to the dataframe
-        dyad_differenced_dataframe = pd.concat([dyad_differenced_dataframe,
-                pd.Series(difference_number)
-                            .append(pd.Series(np.squeeze(np.asarray(whole_frame_movement.flatten()))))
-                            .reset_index(drop=True)])
+        values = pd.Series(np.squeeze(np.asarray(whole_frame_movement.flatten())))        
+        values_varname = 'diff_' + str(difference_number)
+        dyad_differenced_dataframe = pd.concat([dyad_differenced_dataframe, 
+                                                pd.DataFrame({values_varname: values})], 
+                                               axis = 1)
         participant_differenced_dataframe = pd.concat([participant_differenced_dataframe,
-                pd.Series(difference_number)
-                            .append(pd.Series('left'))
-                            .append(pd.Series(person_left_movement))
-                            .reset_index(drop=True)])
-        participant_differenced_dataframe = pd.concat([participant_differenced_dataframe,
-                pd.Series(difference_number)
-                            .append(pd.Series('right'))
-                            .append(pd.Series(person_right_movement))
-                            .reset_index(drop=True)])
-    else:
-        break
+                                                       pd.DataFrame({'difference_number': [difference_number],
+                                                                     'participant': ['left'],
+                                                                     'movement': [person_left_movement]}),
+                                                      pd.DataFrame({'difference_number': [difference_number],
+                                                                     'participant': ['right'],
+                                                                     'movement': [person_right_movement]})])
+        
+        # set the next frame to be the previous one and check if there's another
+        previous_frame = next_frame
+        return_frame_value, frame2 = video_capture.read()
+
+# # loop through frames -- debugging only
+# n = 0
+# while(n < 100):
+
+#     # read in next frame
+#     n += 1
+#     return_frame_value, frame2 = video_capture.read()
+
+#     # read it in if there's something left to read
+#     if (return_frame_value == True):
+#         next_frame = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+#         next_frame = cv2.bilateralFilter(next_frame,9,75,75)
+
+#         # take the absolute difference of the pixel displacement between the frames
+#         differenced_frame = np.matrix(cv2.absdiff(next_frame, previous_frame))
+
+#         # save entire frame's columnwise mean
+#         whole_frame_matrix = differenced_frame[person_height_start:person_height_stop,
+#                                                :]
+#         whole_frame_movement = whole_frame_matrix.mean(axis=1)
+
+#         # carve up left and right matrices
+#         person_left_matrix = differenced_frame[person_height_start:person_height_stop,
+#                                                person_left_x_start:person_left_x_end]
+#         person_right_matrix = differenced_frame[person_height_start:person_height_stop,
+#                                                 person_right_x_start:person_right_x_end]
+
+#         # get average movement for each person
+#         person_left_movement = person_left_matrix.mean()
+#         person_right_movement = person_right_matrix.mean()
+
+#         # increment differenced frame number
+#         difference_number = difference_number + 1
+
+#         # add the current data to the dataframe
+#         values = pd.Series(np.squeeze(np.asarray(whole_frame_movement.flatten())))        
+#         values_varname = 'diff_' + str(difference_number)
+#         dyad_differenced_dataframe = pd.concat([dyad_differenced_dataframe, 
+#                                                 pd.DataFrame({values_varname: values})], 
+#                                                axis = 1)
+#         participant_differenced_dataframe = pd.concat([participant_differenced_dataframe,
+#                                                        pd.DataFrame({'difference_number': [difference_number],
+#                                                                      'participant': ['left'],
+#                                                                      'movement': [person_left_movement]})]).reset_index(drop=True)
+#         participant_differenced_dataframe = pd.concat([participant_differenced_dataframe,
+#                                                        pd.DataFrame({'difference_number': [difference_number],
+#                                                                      'participant': ['right'],
+#                                                                      'movement': [person_right_movement]})]).reset_index(drop=True)
+        
+#         # set the next frame to be the previous one
+#         previous_frame = next_frame
+        
+#     else:
+#         break
 
 # close out the video capture when done
 video_capture.release()
 # cv2.destroyAllWindows()
 
-# rename columns
-#dyad_differenced_dataframe.columns = ['difference_number', ['frame_col'+str(i) for i in range(image_dimensions[0])]]
-#participant_differenced_dataframe.columns = ['difference_number','participant','movement']
+# rename columns for dyad-level dataframe
+dyad_differenced_dataframe = dyad_differenced_dataframe.transpose().reset_index()
+dyad_differenced_dataframe.columns = ['difference_number'] + ['frame_col'+str(i) for i in range(np.size(whole_frame_movement))]
 
 # add the dyad name to the dataframes
-#dyad_differenced_dataframe['dyad'] = conv
-#participant_differenced_dataframe['dyad'] = conv
+dyad_differenced_dataframe['dyad'] = conv
+participant_differenced_dataframe['dyad'] = conv
+participant_differenced_dataframe = participant_differenced_dataframe.reset_index(drop = True)
 
 # write to files
-dyad_differenced_dataframe.to_csv('~/visual_information_in_coordination/data/movement_dataframes-column_differences/movement_data-'+conv+'-raw_dyad.csv',
+dyad_differenced_dataframe.to_csv('./movement_dataframes-column_differences/movement_data-'+conv+'-raw_dyad.csv',
                                  sep=',',index=False, header=True)
-participant_differenced_dataframe.to_csv('~/visual_information_in_coordination/data/movement_dataframes-aggregated/movement_data-'+conv+'-participant.csv',
+participant_differenced_dataframe.to_csv('./movement_dataframes-aggregated/movement_data-'+conv+'-participant.csv',
                                  sep=',',index=False, header=True)
 
 # let us know what's up
-print(next_video_file+ ': Data saved to file')
+print(conv+ ': Data saved to file')
